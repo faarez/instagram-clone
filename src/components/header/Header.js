72715337c6fac08661fba5ea9c8e59c24ad8f5d0
ezekiel <ezekielnizamani0@gmail.com>
2021-05-24 10:26:18 +0200

    created header component and added logo

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="app__header">
+      <img
+        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        className="app__headerImage"
+      />
+    </div>
+  );
+};
+
+export default Header;

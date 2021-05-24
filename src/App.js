9296f765cb3f1464872ef6a941a0ebd50a06f6c2
ezekiel <ezekielnizamani0@gmail.com>
2021-05-24 11:31:51 +0200

    used usestate hook to make more dynamic components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-
+import {useState} from 'react'
 import './App.css';
 import Header from './components/header/Header.js'
 import Post from './components/post/Post.js'
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "ezekiel",
+      caption: "i am back guys",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+    },
+    {
+      username: "dani",
+      caption: "i am back guys",
+      imageUrl:
+        "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    },
+  ]);
+
   return (
     <div className="app">
       <Header />
-      <Post username="ezekiel" caption="i am back guys" imageUrl="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
+      {posts.map((post) => (
+        <Post {...post} />
+      ))}
+      {/* <Post username="ezekiel" caption="i am back guys" imageUrl="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
       <Post username="dani" caption="my new vlog is out" imageUrl="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
-      <Post username="hasham" caption="this my first post" imageUrl="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" />
+      <Post username="hasham" caption="this my first post" imageUrl="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" /> */}
     </div>
   );
 }

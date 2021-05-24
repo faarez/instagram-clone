401c4bdc4ce28d0dec693ad4a902e002b8279507
ezekiel <ezekielnizamani0@gmail.com>
2021-05-24 11:20:26 +0200

    styled  post component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Post from './components/post/Post.js'
 function App() {
   return (
     <div className="app">
-    <Header/>
-    <Post/>
+      <Header />
+      <Post username="ezekiel" caption="i am back guys" imageUrl="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
+      <Post username="dani" caption="my new vlog is out" imageUrl="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
+      <Post username="hasham" caption="this my first post" imageUrl="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" />
     </div>
   );
 }
--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,26 +1,26 @@
 import "./Post.css";
 import {Avatar} from '@material-ui/core'
-const Post = () => {
+const Post = ({imageUrl,username,caption}) => {
   return (
     <div className="post">
       <div className="post__header">
         <Avatar
-          alt="Yoga.ezekeil"
+          alt={username}
           className="post__avatar"
           src="/static/images/avatar/1.jpg"
         />
-        <h3>Username</h3>
+        <h3>{username}</h3>
       </div>
       {/* Header -> avatar + username */}
 
       <img
         className="post__image"
         alt="tree "
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+        src={imageUrl}
       />
       {/* Image */}
       <h4 className="post__text">
-        <strong>Yoga.ezekeil</strong> love this
+        <strong>{username}</strong> {caption}
       </h4>
       {/* username + caption */}
     </div>
